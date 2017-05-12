fe7f19e5e7985b161c06b32d758f3822cb986180
peterderivaz <peter.derivaz@gmail.com>
2017-05-12 17:11:57 +0100

    Use local function to simplify MakeString code

--- src/main/scala/alogic/StrTree.scala
+++ src/main/scala/alogic/StrTree.scala
@@ -9,20 +9,22 @@ case class StrList(names: List[StrTree]) extends StrTree
 case class StrCommaList(names: List[StrTree]) extends StrTree
 
 object MakeString {
-  def go(tree: StrTree, b: StringBuilder): Unit = tree match {
-    case Str(name) => b.append(name)
-    case StrList(names) => for ( n <- names ) go(n,b)
-    case StrCommaList(names) => {
-      go(names.head,b)
-      for ( n <- names.tail ) {
-        b.append(",")
-        go(n,b)
-      }
-    }
-  }
   def apply(tree: StrTree): String = {
     val b = StringBuilder.newBuilder
-    go(tree,b)
-    b.toString()
+    
+    def go(tree: StrTree): Unit = tree match {
+      case Str(name) => b.append(name)
+      case StrList(names) => names foreach go
+      case StrCommaList(names) => {
+        go(names.head)
+        for ( n <- names.tail ) {
+          b.append(",")
+          go(n)
+        }
+      }
+    }
+    
+    go(tree)
+    b.toString
   }
 }
