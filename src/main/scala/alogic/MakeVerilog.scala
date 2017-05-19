3bfe384fb50054d6c5fb8b7d1dc2570b51f67cb9
peterderivaz <peter.derivaz@gmail.com>
2017-05-19 09:33:19 +0100

    Correcting indent and emitting combinatorial conditions

--- src/main/scala/alogic/MakeVerilog.scala
+++ src/main/scala/alogic/MakeVerilog.scala
@@ -57,25 +57,26 @@ final class MakeVerilog {
     def writeSigned(signed: Boolean) = if (signed) "signed " else ""
 
     def writeOut(typ: AlogicType, name: StrTree): Unit = typ match {
-      case IntType(b, 1)    => pw.println(s"out reg ${writeSigned(b)}" + MakeString(name) + ";")
-      case IntType(b, size) => pw.println(s"out reg ${writeSigned(b)}[$size-1:0]" + MakeString(name) + ";")
+      case IntType(b, 1)    => pw.println(s"  output reg ${writeSigned(b)}" + MakeString(name) + ";")
+      case IntType(b, size) => pw.println(s"  output reg ${writeSigned(b)}[$size-1:0] " + MakeString(name) + ";")
       case _                => // TODO
     }
     def writeIn(typ: AlogicType, name: StrTree): Unit = typ match {
-      case IntType(b, 1)    => pw.println(s"in wire ${writeSigned(b)}" + MakeString(name) + ";")
-      case IntType(b, size) => pw.println(s"in wire ${writeSigned(b)}[$size-1:0]" + MakeString(name) + ";")
+      case IntType(b, 1)    => pw.println(s"  input wire ${writeSigned(b)}" + MakeString(name) + ";")
+      case IntType(b, size) => pw.println(s"  input wire ${writeSigned(b)}[$size-1:0] " + MakeString(name) + ";")
       case _                => // TODO
     }
     def writeVar(typ: AlogicType, name: StrTree): Unit = {
       typ match {
-        case IntType(b, 1)    => pw.println(s"reg ${writeSigned(b)}" + MakeString(name) + ";")
-        case IntType(b, size) => pw.println(s"reg ${writeSigned(b)}[$size-1:0]" + MakeString(name) + ";")
+        case IntType(b, 1)    => pw.println(s"  reg ${writeSigned(b)}" + MakeString(name) + ";")
+        case IntType(b, size) => pw.println(s"  reg ${writeSigned(b)}[$size-1:0] " + MakeString(name) + ";")
         case _                => // TODO variable int type
       }
       typ match {
         case IntType(a, num) => {
           resets = StrList(Str("      ") :: Str(reg(name)) :: Str(s" <= $num'b0;\n") :: Nil) :: resets
-          clocks = StrList(Str("      ") :: Str(reg(name)) :: Str(" <= ") :: Str(nx(name)) :: Str(";\n") :: Nil) :: resets
+          clocks = StrList(Str("        ") :: Str(reg(name)) :: Str(" <= ") :: Str(nx(name)) :: Str(";\n") :: Nil) :: clocks
+          defaults = StrList(Str("    ") :: Str(nx(name)) :: Str(" = ") :: Str(reg(name)) :: Str(";\n") :: Nil) :: defaults
         }
         case _ =>
       }
@@ -84,32 +85,36 @@ final class MakeVerilog {
     VisitAST(tree) {
       case Task(Fsm(), name, decls, fns) => {
         pw.println(s"module $name (")
+        pw.println("  input wire clk,")
+        pw.println("  input wire rst_n,")
+        // TODO if a network or a verilog module these should all be wires
+        // TODO skip auto stuff if we have a verilog task
         id2decl.values.foreach({
           case ParamDeclaration(decltype, id, Some(init)) => pw.println("param " + id + " = " + MakeString(MakeExpr(init)) + ";")
           case ParamDeclaration(decltype, id, None)       => pw.println("param " + id + ";")
           case OutDeclaration(synctype, decltype, name) => {
             if (HasValid(synctype)) {
-              pw.println("out reg " + valid(name) + ";")
-              clears = Str("    " + valid(name) + " = 1'b0;\n") :: clears
+              pw.println("  output reg " + valid(name) + ";")
+              clears = Str("      " + valid(name) + " = 1'b0;\n") :: clears
               defaults = Str("    " + valid(name) + " = 1'b0;\n") :: defaults
             }
             if (HasReady(synctype))
-              pw.println("in wire " + ready(name) + ";")
+              pw.println("  input wire " + ready(name) + ";")
             if (HasAccept(synctype))
-              pw.println("out reg " + accept(name) + ";")
+              pw.println("  output reg " + accept(name) + ";")
             VisitType(decltype, name)(writeOut)
 
           }
           case InDeclaration(synctype, decltype, name) => {
             if (HasValid(synctype))
-              pw.println("in wire " + valid(name) + ";")
+              pw.println("  input wire " + valid(name) + ";")
             if (HasReady(synctype)) {
-              pw.println("out reg " + ready(name) + ";")
-              clears = Str("    " + ready(name) + " = 1'b0;\n") :: clears
+              pw.println("  output reg " + ready(name) + ";")
+              clears = Str("      " + ready(name) + " = 1'b0;\n") :: clears
               defaults = Str("    " + ready(name) + " = 1'b0;\n") :: defaults
             }
             if (HasAccept(synctype))
-              pw.println("in wire " + accept(name) + ";")
+              pw.println("  in wire " + accept(name) + ";")
             VisitType(decltype, name)(writeOut)
           }
           case _ =>
@@ -127,42 +132,46 @@ final class MakeVerilog {
       case _                    => true
     }
     // Start main combinatorial loop
-    pw.println("always @* begin")
-    pw.println("  go = 1'b1;")
+    pw.println()
+    pw.println("  always @* begin")
+    pw.println("    go = 1'b1;")
     // Prepare defaults
-    pw.println(MakeString(StrList(defaults)))
-    pw.println(MakeString(StrList(fencefns)))
-    pw.println("  case(state) begin")
-    pw.println("    default: begin")
+    if (defaults.length > 0)
+      pw.println(MakeString(StrList(defaults)))
+    if (fencefns.length > 0)
+      pw.println(MakeString(StrList(fencefns)))
+    pw.println("    case(state) begin")
+    pw.println("      default: begin")
     pw.println(MakeString(StrList(fns)))
+    pw.println("      end")
     pw.println("    end")
-    pw.println("  end")
     if (clears.length > 0) {
-      pw.println(s"  if (!$go) begin")
+      pw.println(s"    if (!$go) begin")
       pw.write(MakeString(clears))
-      pw.println("  end")
+      pw.println("    end")
     }
-    pw.println("end")
+    pw.println("  end")
     // Now emit clocked blocks
-    pw.println("always @(posedge clk or negedge rst_n) begin")
-    pw.println("  if (!rst_n) begin")
-    pw.println(MakeString(StrList(resets)))
-    pw.println("  end else begin")
-    pw.println(s"    if ($go) begin")
-    pw.println(MakeString(StrList(clocks)))
+    pw.println("")
+    pw.println("  always @(posedge clk or negedge rst_n) begin")
+    pw.println("    if (!rst_n) begin")
+    pw.print(MakeString(StrList(resets)))
+    pw.println("    end else begin")
+    pw.println(s"      if ($go) begin")
+    pw.print(MakeString(StrList(clocks)))
+    pw.println("      end")
     pw.println("    end")
     pw.println("  end")
-    pw.println("end")
     pw.println("endmodule")
     pw.close()
   }
 
   // Construct the string to be used when this identifier is used on the RHS of an assignment
-  def nx(names: List[String]): String = names.mkString("_") // TODO inspect type and expand
+  def nx(names: List[String]): String = names.mkString("_") + "_d" // TODO inspect type and expand
   def nx(name: StrTree): String = MakeString(name) + "_d"
 
   // Construct the string to be used when this identifier is used on the LHS of an assignment
-  def reg(names: List[String]): String = names.mkString // TODO inspect type and expand
+  def reg(names: List[String]): String = names.mkString("_") + "_q" // TODO inspect type and expand
   def reg(name: StrTree): String = MakeString(name) + "_q"
 
   implicit def string2StrTree(s: String): StrTree = Str(s)
@@ -178,10 +187,10 @@ final class MakeVerilog {
       // TODO     case Sxt(numbits, expr) => {VisitAST(numbits,callback); VisitAST(expr,callback)}
       case DollarCall(name, args)                => StrList(List(name, "(", StrCommaList(args.map(MakeExpr)), ")"))
       case ReadCall(name, args)                  => MakeExpr(name)
-      case BinaryOp(lhs, op, rhs)                => StrList(List(MakeExpr(lhs), op, MakeExpr(rhs)))
+      case BinaryOp(lhs, op, rhs)                => StrList(List(MakeExpr(lhs), Str(" "), op, Str(" "), MakeExpr(rhs)))
       case UnaryOp(op, lhs)                      => StrList(List(op, MakeExpr(lhs)))
       case Bracket(content)                      => StrList(List("(", MakeExpr(content), ")"))
-      case TernaryOp(cond, lhs, rhs)             => StrList(List(MakeExpr(cond), "?", MakeExpr(lhs), ":", MakeExpr(rhs)))
+      case TernaryOp(cond, lhs, rhs)             => StrList(List(MakeExpr(cond), " ? ", MakeExpr(lhs), " : ", MakeExpr(rhs)))
       case BitRep(count, value)                  => StrList(List("{", MakeExpr(count), "{", MakeExpr(value), "}}"))
       case BitCat(parts)                         => StrList(List("{", StrCommaList(parts.map(MakeExpr)), "}"))
       case DottedName(names)                     => nx(names)
@@ -207,9 +216,9 @@ final class MakeVerilog {
       d match {
         case InDeclaration(synctype, _, _) => {
           if (HasValid(synctype))
-            add(s"$go = $go && ${valid(n)};")
+            add(s"$go = $go && ${valid(n)};\n")
           if (!isLock && HasReady(synctype))
-            add(ready(n) + " = 1'b1;")
+            add(ready(n) + " = 1'b1;\n")
         }
         case _ => error(s"$name cannot be read"); false // TODO check this earlier?
       }
@@ -258,42 +267,46 @@ final class MakeVerilog {
 
   def AddStall(indent: Int, stalls: List[String], expr: StrTree): StrTree = {
     if (stalls.isEmpty)
-      StrList(" " * indent :: expr :: Nil)
+      expr
     else
       StrList(Str(" " * indent) :: Str("begin\n") ::
-        stalls.map(x => Str(" " * (indent + 4) + x)) :::
-        Str(" " * indent) :: expr ::
+        stalls.map(x => Str(" " * (indent) + x)) :::
+        expr ::
         Str(" " * indent) :: Str("end\n") :: Nil)
   }
 
   // Produce code to go into the case statements (we assume we have already had a "case(state) default: begin"
   // The top call should be with Function or VerilogFunction
   def CombStmt(indent: Int, tree: AlogicAST): StrTree = tree match {
-    case Assign(lhs, "=", rhs) => AddStall(indent, StallExpr(lhs) ::: StallExpr(rhs), StrList(List(MakeExpr(lhs), "=", MakeExpr(rhs))))
+    case Assign(lhs, "=", rhs) => AddStall(indent, StallExpr(lhs) ::: StallExpr(rhs), StrList(List(Str(" " * indent), MakeExpr(lhs), " = ", MakeExpr(rhs), ";\n")))
     case CombinatorialCaseStmt(value, cases) => AddStall(indent, StallExpr(value),
-      StrList(Str(" " * indent) :: Str("case(") :: MakeExpr(value) :: Str(") begin\n") ::
+      StrList(Str(" " * indent + "case(") :: MakeExpr(value) :: Str(") begin\n") ::
         StrList(for (c <- cases) yield CombStmt(indent + 4, c)) ::
-        Str(" " * indent) :: Str("end\n") :: Nil))
+        Str(" " * indent) :: Str("endcase\n") :: Nil))
     case CombinatorialIf(cond, body, Some(elsebody)) => AddStall(indent, StallExpr(cond),
-      StrList(Str(" " * indent) :: Str("if\n") ::
+      StrList(Str(" " * indent) :: Str("if (") :: MakeExpr(cond) :: Str(")\n") ::
         CombStmt(indent + 4, body) ::
         Str(" " * indent) :: Str("else\n") ::
         CombStmt(indent + 4, elsebody) :: Nil))
     case CombinatorialIf(cond, body, None) => AddStall(indent, StallExpr(cond),
       StrList(Str(" " * indent) :: Str("if\n") ::
         CombStmt(indent + 4, body) :: Nil))
-    case LockCall(_, _)                              => Str("")
-    case UnlockCall(_, _)                            => Str("")
-    case WriteCall(name, args) if (args.length == 1) => AddStall(indent, StallExpr(name) ::: StallExpr(args(0)), StrList(List(MakeExpr(name), "=", MakeExpr(args(0)))))
+    case LockCall(_, _)   => Str("")
+    case UnlockCall(_, _) => Str("")
+    case WriteCall(name, args) if (args.length == 1) => AddStall(indent, StallExpr(name) ::: StallExpr(args(0)),
+      StrList(List(Str(" " * indent), MakeExpr(name), " = ", MakeExpr(args(0)), Str(";\n"))))
     case CombinatorialBlock(cmds) => StrList(Str(" " * indent) :: Str("begin\n") ::
       StrList(for { cmd <- cmds } yield CombStmt(indent + 4, cmd)) ::
       Str(" " * indent) :: Str("end\n") :: Nil)
     case DeclarationStmt(VarDeclaration(decltype, id, Some(rhs))) => CombStmt(indent, Assign(id, "=", rhs))
-    case DeclarationStmt(VarDeclaration(decltype, id, None)) => CombStmt(indent, Assign(id, "=", Num("'b0")))
-    case Plusplus(lhs) => CombStmt(indent, Assign(lhs, "=", BinaryOp(lhs, "+", Num("1'b1"))))
-    case AlogicComment(s) => s"// $s\n"
-    case StateStmt(state) => StrList(List(" " * (indent - 4), "end\n", " " * (indent - 4), MakeState(state), ": begin\n"))
-    case GotoState(target) => StrList(List(" " * indent, nx("state"), "=", MakeState(target), ";\n"))
+    case DeclarationStmt(VarDeclaration(decltype, id, None))      => CombStmt(indent, Assign(id, "=", Num("'b0")))
+    case AlogicComment(s)                                         => s"// $s\n"
+    case StateStmt(state)                                         => StrList(List(" " * (indent - 4), "end\n", " " * (indent - 4), MakeState(state), ": begin\n"))
+    case GotoState(target)                                        => StrList(List(" " * indent, nx("state"), " = ", MakeState(target), ";\n"))
+    case CombinatorialCaseLabel(Nil, body) => StrList(
+      Str(" " * indent) ::
+        Str("default:\n") ::
+        CombStmt(indent + 4, body) :: Nil)
     case CombinatorialCaseLabel(conds, body) => StrList(
       Str(" " * indent) ::
         StrCommaList(conds.map(MakeExpr)) ::
