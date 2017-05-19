3e38947c1416fbccb8e719777242e8d733e96975
peterderivaz <peter.derivaz@gmail.com>
2017-05-19 14:49:57 +0100

    Add support for >>= and friends

--- src/main/scala/alogic/Desugar.scala
+++ src/main/scala/alogic/Desugar.scala
@@ -5,10 +5,10 @@ import AstOps._
 object Desugar {
   def RemoveAssigns(tree: StateProgram): StateProgram = {
     val ast = RewriteAST(tree) {
-      case Assign(lhs, op, rhs) if (op.length == 2) => Some(Assign(lhs, "=", BinaryOp(Bracket(lhs), op.substring(0, 1), Bracket(rhs))))
-      case Plusplus(lhs)                            => Some(Assign(lhs, "=", BinaryOp(Bracket(lhs), "+", Num("1'b1"))))
-      case Minusminus(lhs)                          => Some(Assign(lhs, "=", BinaryOp(Bracket(lhs), "-", Num("1'b1"))))
-      case _                                        => None
+      case Assign(lhs, op, rhs) if (op.length > 1) => Some(Assign(lhs, "=", BinaryOp(Bracket(lhs), op.substring(0, op.length - 1), Bracket(rhs))))
+      case Plusplus(lhs)                           => Some(Assign(lhs, "=", BinaryOp(Bracket(lhs), "+", Num("1'b1"))))
+      case Minusminus(lhs)                         => Some(Assign(lhs, "=", BinaryOp(Bracket(lhs), "-", Num("1'b1"))))
+      case _                                       => None
     }
     ast.asInstanceOf[StateProgram]
   }
