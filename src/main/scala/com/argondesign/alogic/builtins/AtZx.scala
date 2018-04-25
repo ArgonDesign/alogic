18e768576e92eab165ec2b9f687ec8580c122919
Geza Lore <gezalore@gmail.com>
2018-04-26 00:21:21 +0100

    Implement @zx on symbol by folding

--- src/main/scala/com/argondesign/alogic/builtins/AtZx.scala
+++ src/main/scala/com/argondesign/alogic/builtins/AtZx.scala
@@ -17,6 +17,7 @@ package com.argondesign.alogic.builtins
 
 import com.argondesign.alogic.ast.Trees._
 import com.argondesign.alogic.core.CompilerContext
+import com.argondesign.alogic.core.Loc
 import com.argondesign.alogic.core.Types._
 
 object AtZx extends BuiltinPolyFunc {
@@ -32,29 +33,62 @@ object AtZx extends BuiltinPolyFunc {
   }
 
   private[builtins] override def fold(call: ExprCall)(implicit cc: CompilerContext): Expr = {
-    val folded = call.args match {
-      case List(width: ExprNum, value: ExprInt) => foldNumInt(call, width, value)
-      case _                                    => None
-    }
-
-    folded map { expr =>
+    call.args(0).value flatMap { dstWidth =>
+      call.args(1) match {
+        case int: ExprInt => foldNumInt(call.loc, dstWidth, int)
+        case ref: ExprRef => foldNumRef(call.loc, dstWidth, ref)
+        case _            => None
+      }
+    } map { expr =>
       if (!expr.hasLoc) expr withLoc call.loc else expr
-    } getOrElse call
+    } getOrElse {
+      call
+    }
   }
 
-  private def foldNumInt(call: ExprCall, width: ExprNum, value: ExprInt)(
-      implicit cc: CompilerContext): Option[Expr] = {
-    if (width.value > value.width) {
-      val mask = (BigInt(1) << value.width) - 1
-      Some(ExprInt(value.signed, width.value.toInt, value.value & mask))
-    } else if (width.value == value.width) {
-      Some(value)
-    } else {
-      cc.error(
-        call,
-        s"Result width (${width.value}) of ${name} is less than argument width (${value.width})"
-      )
-      Some(ExprError())
+  private def foldNumInt(
+      loc: Loc,
+      width: BigInt,
+      value: ExprInt
+  )(
+      implicit cc: CompilerContext
+  ): Option[Expr] = {
+    Some {
+      if (width > value.width) {
+        val mask = (BigInt(1) << value.width) - 1
+        ExprInt(value.signed, width.toInt, value.value & mask)
+      } else if (width == value.width) {
+        value
+      } else {
+        cc.error(
+          loc,
+          s"Result width (${width}) of ${name} is less than argument width (${value.width})"
+        )
+        ExprError()
+      }
+    }
+  }
+
+  private def foldNumRef(
+      loc: Loc,
+      dstWidth: BigInt,
+      ref: ExprRef
+  )(
+      implicit cc: CompilerContext
+  ): Option[Expr] = {
+    val ExprRef(Sym(symbol)) = ref
+    symbol.denot.kind.width.value map { srcWidth =>
+      if (dstWidth > srcWidth) {
+        ExprCat(List(ExprInt(false, (dstWidth - srcWidth).toInt, 0), ref)) regularize loc
+      } else if (dstWidth == srcWidth) {
+        ref
+      } else {
+        cc.error(
+          loc,
+          s"Result width (${dstWidth}) of ${name} is less than argument width (${srcWidth})"
+        )
+        ExprError()
+      }
     }
   }
 
