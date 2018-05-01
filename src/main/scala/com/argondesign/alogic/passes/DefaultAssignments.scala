8d4fceb8181d7a76cd1e6d8c5cb7c10be8c07c69
Geza Lore <gezalore@gmail.com>
2018-05-01 20:44:16 +0100

    Remove liveness check from DefaultAssignments (need to be bitwise..)

--- src/main/scala/com/argondesign/alogic/passes/DefaultAssignments.scala
+++ src/main/scala/com/argondesign/alogic/passes/DefaultAssignments.scala
@@ -27,7 +27,6 @@ import com.argondesign.alogic.core.Symbols._
 import com.argondesign.alogic.core.Types._
 import com.argondesign.alogic.typer.TypeAssigner
 import com.argondesign.alogic.util.FollowedBy
-import com.argondesign.alogic.util.unreachable
 
 import scala.collection.mutable
 
@@ -36,26 +35,6 @@ final class DefaultAssignments(implicit cc: CompilerContext)
     with FollowedBy {
 
   private val needsDefault = mutable.Set[TermSymbol]()
-
-  // If 'lval' is true, given an expression, return an iterable of symbols that
-  // would be read if this expression is used on on the left hand side of an
-  // assignment. If 'lval' is false, return an iterable of all symbols
-  // referenced (i.e.: read) in the expression
-  private def readSymbols(lval: Boolean)(expr: Expr): Iterator[TermSymbol] = {
-    if (!lval) {
-      expr collect { case ExprRef(Sym(symbol: TermSymbol)) => symbol }
-    } else {
-      expr match {
-        case _: ExprRef        => Iterator.empty
-        case ExprCat(parts)    => parts.toIterator flatMap readSymbols(lval = true)
-        case ExprIndex(_, idx) => readSymbols(lval = false)(idx)
-        case ExprSlice(_, lidx, _, ridx) => {
-          readSymbols(lval = false)(lidx) ++ readSymbols(lval = false)(ridx)
-        }
-        case _ => unreachable
-      }
-    }
-  }
 
   // Given an expression, return an iterable of symbols that would be assigned
   // should this expression be used on the left hand side of an assignment
@@ -85,108 +64,6 @@ final class DefaultAssignments(implicit cc: CompilerContext)
     case _ =>
   }
 
-  // Compute the liveness of a symbol through a list of statements. Returns:
-  // - Some(true) iff there exists at least 1 path where the symbol
-  //   is read before being writen (i.e.: the symbol is alive)
-  // - Some(false) iff for all paths the symbol
-  //   is written before being read (i.e.: the symbol is dead)
-  // - None otherwise, which can happen if there are no reads, but there
-  //   might be some writes. In this case, liveness is determined by the
-  //   successors of these statements.
-  def isAlive(symbol: TermSymbol, stmts: List[Stmt]): Option[Boolean] = {
-
-    // Given the liveness across the parallel paths of a branching statement,
-    // compute the liveness of the branching statement itself
-    def combine(liveness: Iterator[Option[Boolean]]): Option[Boolean] = {
-      def checkNext(): Option[Boolean] = {
-        if (!liveness.hasNext) {
-          None
-        } else {
-          liveness.next() match {
-            // If it's alive at this branch, we can quickly conclude it's alive
-            case option @ Some(true) => option
-            // If if's dead at this branch, liveness is up to the other
-            // branches. If there aren't any more, we conclude it's dead
-            case option @ Some(false) => if (!liveness.hasNext) option else checkNext()
-            // If it's liveness is indeterminate at this branch, then we
-            // know it cannot be dead, but it might still be alive if it
-            // is alive across any of the other branches
-            case None => {
-              checkNext() match {
-                case option @ Some(true) => option
-                case _                   => None
-              }
-            }
-          }
-        }
-      }
-      checkNext()
-    }
-
-    // Recursive lazy implementation
-    def check(stmts: List[Stmt]): Option[Boolean] = {
-      if (stmts.isEmpty) {
-        None
-      } else {
-        val head :: tail = stmts
-
-        val here: Option[Boolean] = head match {
-          case StmtAssign(lhs, rhs) => {
-            if (readSymbols(lval = false)(rhs) contains symbol) {
-              Some(true)
-            } else if (readSymbols(lval = true)(lhs) contains symbol) {
-              Some(true)
-            } else if (writtenSymbols(lhs) contains symbol) {
-              Some(false)
-            } else {
-              None
-            }
-          }
-
-          case StmtIf(cond, thenStmt, elseStmtOpt) => {
-            if (readSymbols(lval = false)(cond) contains symbol) {
-              Some(true)
-            } else {
-              combine {
-                Iterator.single(check(List(thenStmt))) ++
-                  Iterator.single(check(elseStmtOpt.toList))
-              }
-            }
-          }
-
-          case StmtCase(expr, cases, default) => {
-            if (readSymbols(lval = false)(expr) contains symbol) {
-              Some(true)
-            } else {
-              val alive = cases.view flatMap {
-                case CaseClause(exprs, _) => exprs
-              } exists { expr =>
-                readSymbols(lval = false)(expr) contains symbol
-              }
-              if (alive) {
-                Some(true)
-              } else {
-                combine {
-                  Iterator.single(check(default)) ++
-                    (cases.iterator map { case CaseClause(_, body) => check(List(body)) })
-                }
-              }
-            }
-          }
-
-          case StmtBlock(body) => check(body)
-          case _: StmtStall    => None
-          case _: StmtFence    => None
-          case _               => unreachable
-        }
-
-        here orElse check(tail)
-      }
-    }
-
-    check(stmts)
-  }
-
   override def transform(tree: Tree): Tree = tree match {
     // TODO: skip early for verbatim entities
     case entity: Entity if needsDefault.nonEmpty && entity.variant != "verbatim" => {
@@ -196,32 +73,6 @@ final class DefaultAssignments(implicit cc: CompilerContext)
           case ExprRef(Sym(symbol: TermSymbol)) => {
             needsDefault remove symbol
           }
-        }
-      }
-
-      if (needsDefault.nonEmpty) {
-        assert(entity.states.nonEmpty)
-        // TODO: This now walks the tree multiple times, once for each symbol,
-        // it should just compute the alive/dead sets in one pass...
-
-        // Remove symbols that are dead at the beginning of the cycle. To do
-        // this, we build the case statement representing the state dispatch
-        // (together with the fence statements), and do liveness analysis on it
-        val stateSystem = if (entity.states.lengthCompare(1) == 0) {
-          entity.fenceStmts ::: entity.states.head.body
-        } else {
-          entity.fenceStmts :+ StmtCase(
-            ExprRef(Sym(entitySymbol.attr.stateVar.value)),
-            entity.states.tail map {
-              case State(expr, body) => CaseClause(List(expr), StmtBlock(body))
-            },
-            entity.states.head.body
-          )
-        }
-
-        // Now retain only the symbols that are not dead
-        needsDefault retain { symbol =>
-          !(isAlive(symbol, stateSystem) contains false)
         }
       }
 
