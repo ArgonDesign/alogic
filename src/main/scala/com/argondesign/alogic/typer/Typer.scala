f81f7aa15107a55ecb102bcd7c185f631a77e9fe
Geza Lore <gezalore@gmail.com>
2019-06-17 20:33:09 +0100

    Allow TypeNum as function arguments

--- src/main/scala/com/argondesign/alogic/typer/Typer.scala
+++ src/main/scala/com/argondesign/alogic/typer/Typer.scala
@@ -440,20 +440,25 @@ final class Typer(externalRefs: Boolean = false)(implicit cc: CompilerContext)
         require(args forall { _.hasTpe })
 
         def checkArg(expected: Type, arg: Expr, i: Int): Tree = {
-          assert(expected.isPacked)
-          checkPacked(arg, s"Argument ${i} to function call") map {
+          assert(expected.isPacked || expected.isNum)
+          checkNumericOrPacked(arg, s"Argument ${i} to function call") map {
             ExprError() withLoc _
           } getOrElse {
-            val argWidth = arg.tpe.width
-            val expWidth = expected.width
-            if (argWidth != expWidth) {
-              val cmp = if (argWidth > expWidth) "greater" else "less"
-              cc.error(
-                arg,
-                s"Width ${argWidth} of argument ${i} passed to function call is ${cmp} than expected width ${expWidth}")
-              ExprError() withLoc expr.loc
-            } else {
+            if (expected.isNum || arg.tpe.isNum) {
+              // TODO: check signedness?
               tree
+            } else {
+              val argWidth = arg.tpe.width
+              val expWidth = expected.width
+              if (argWidth != expWidth) {
+                val cmp = if (argWidth > expWidth) "greater" else "less"
+                cc.error(
+                  arg,
+                  s"Width ${argWidth} of argument ${i} passed to function call is ${cmp} than expected width ${expWidth}")
+                ExprError() withLoc expr.loc
+              } else {
+                tree
+              }
             }
           }
         }
