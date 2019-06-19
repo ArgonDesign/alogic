b2cfdd0a46f6e41fb2a23b645bf454607cb226bf
Geza Lore <gezalore@gmail.com>
2019-06-19 16:10:09 +0100

    Change AddImplicitCasts tests to desired behaviour

--- src/test/scala/com/argondesign/alogic/typer/AddImplicitCastsSpec.scala
+++ src/test/scala/com/argondesign/alogic/typer/AddImplicitCastsSpec.scala
@@ -55,10 +55,10 @@ final class AddImplicitCastsSpec extends FreeSpec with AlogicTest {
             (text, res) <- List(
               (s"8'd3 ${op} 2", Some(Right(ExprCast(TypeUInt(8), 2)))),
               (s"2 ${op} 8'd3", Some(Left(ExprCast(TypeUInt(8), 2)))),
-              (s"8'd3 ${op} -'sd2", Some(Right(ExprCast(TypeUInt(8), ExprNum(true, -2))))),
-              (s"-'sd2 ${op} 8'd3", Some(Left(ExprCast(TypeUInt(8), ExprNum(true, -2))))),
-              (s"7'sd3 ${op} 2", Some(Right(ExprCast(TypeSInt(7), 2)))),
-              (s"2 ${op} 7'sd3", Some(Left(ExprCast(TypeSInt(7), 2)))),
+              (s"8'd3 ${op} -'sd2", Some(Right(ExprCast(TypeSInt(8), ExprNum(true, -2))))),
+              (s"-'sd2 ${op} 8'd3", Some(Left(ExprCast(TypeSInt(8), ExprNum(true, -2))))),
+              (s"7'sd3 ${op} 2", Some(Right(ExprCast(TypeUInt(7), 2)))),
+              (s"2 ${op} 7'sd3", Some(Left(ExprCast(TypeUInt(7), 2)))),
               (s"7'sd3 ${op} -'sd2", Some(Right(ExprCast(TypeSInt(7), ExprNum(true, -2))))),
               (s"-'sd2 ${op} 7'sd3", Some(Left(ExprCast(TypeSInt(7), ExprNum(true, -2))))),
               (s"4 ${op} 2 ", None)
@@ -83,8 +83,12 @@ final class AddImplicitCastsSpec extends FreeSpec with AlogicTest {
           (text, res) <- List(
             ("0 ? 0 : 2'd1", Some(Left(ExprCast(TypeUInt(2), 0)))),
             ("0 ? 2'd1 : 0", Some(Right(ExprCast(TypeUInt(2), 0)))),
-            ("0 ? 0 : 3'sd1", Some(Left(ExprCast(TypeSInt(3), 0)))),
-            ("0 ? 3'sd1 : 0", Some(Right(ExprCast(TypeSInt(3), 0)))),
+            ("0 ? 0 : 3'sd1", Some(Left(ExprCast(TypeUInt(3), 0)))),
+            ("0 ? 3'sd1 : 0", Some(Right(ExprCast(TypeUInt(3), 0)))),
+            ("0 ? 'sd0 : 2'd1", Some(Left(ExprCast(TypeSInt(2), 0)))),
+            ("0 ? 2'd1 : 'sd0", Some(Right(ExprCast(TypeSInt(2), 0)))),
+            ("0 ? 'sd0 : 3'sd1", Some(Left(ExprCast(TypeSInt(3), 0)))),
+            ("0 ? 3'sd1 : 'sd0", Some(Right(ExprCast(TypeSInt(3), 0)))),
             ("0 ? 1 : 0", None)
           )
         } {
@@ -112,14 +116,14 @@ final class AddImplicitCastsSpec extends FreeSpec with AlogicTest {
 
         for {
           (index, res) <- List(
+            // format: off
             ("u8 a; (* unused *) u1 b = a[0]", List(ExprCast(TypeUInt(3), 0))),
             ("u9 a; (* unused *) u1 b = a[0]", List(ExprCast(TypeUInt(4), 0))),
             ("u32[8] a; (* unused *) u32 b = a[0]", List(ExprCast(TypeUInt(3), 0))),
             ("u33[9] a; (* unused *) u33 b = a[0]", List(ExprCast(TypeUInt(4), 0))),
-            ("u32[8] a; (* unused *) u1 b = a[0][2]",
-             List(ExprCast(TypeUInt(5), 2), ExprCast(TypeUInt(3), 0))),
-            ("u33[9] a; (* unused *) u1 b = a[0][2]",
-             List(ExprCast(TypeUInt(6), 2), ExprCast(TypeUInt(4), 0))),
+            ("u32[8] a; (* unused *) u1 b = a[0][2]", List(ExprCast(TypeUInt(5), 2), ExprCast(TypeUInt(3), 0))),
+            ("u33[9] a; (* unused *) u1 b = a[0][2]", List(ExprCast(TypeUInt(6), 2), ExprCast(TypeUInt(4), 0)))
+            // format: on
           )
         } {
           index in {
@@ -148,48 +152,20 @@ final class AddImplicitCastsSpec extends FreeSpec with AlogicTest {
 
         for {
           (slice, res) <- List(
-            ("u8 a; (* unused *) u2 b = a[1:0]",
-             List((ExprCast(TypeUInt(3), 1), ExprCast(TypeUInt(3), 0)))),
-            ("u9 a; (* unused *) u2 b = a[1:0]",
-             List((ExprCast(TypeUInt(4), 1), ExprCast(TypeUInt(4), 0)))),
-            ("u8 a; (* unused *) u1 b = a[2+:1]",
-             List((ExprCast(TypeUInt(3), 2), ExprCast(TypeUInt(3), 1)))),
-            ("u9 a; (* unused *) u1 b = a[2+:1]",
-             List((ExprCast(TypeUInt(4), 2), ExprCast(TypeUInt(4), 1)))),
-            ("u8 a; (* unused *) u1 b = a[2-:1]",
-             List((ExprCast(TypeUInt(3), 2), ExprCast(TypeUInt(3), 1)))),
-            ("u9 a; (* unused *) u1 b = a[2-:1]",
-             List((ExprCast(TypeUInt(4), 2), ExprCast(TypeUInt(4), 1)))),
-            ("u32[8] a; (* unused *) u2 b = a[1:0]",
-             List((ExprCast(TypeUInt(3), 1), ExprCast(TypeUInt(3), 0)))),
-            ("u33[9] a; (* unused *) u2 b = a[1:0]",
-             List((ExprCast(TypeUInt(4), 1), ExprCast(TypeUInt(4), 0)))),
-            ("u32[8] a; (* unused *) u1 b = a[2+:1]",
-             List((ExprCast(TypeUInt(3), 2), ExprCast(TypeUInt(3), 1)))),
-            ("u33[9] a; (* unused *) u1 b = a[2+:1]",
-             List((ExprCast(TypeUInt(4), 2), ExprCast(TypeUInt(4), 1)))),
-            ("u32[8] a; (* unused *) u1 b = a[2-:1]",
-             List((ExprCast(TypeUInt(3), 2), ExprCast(TypeUInt(3), 1)))),
-            ("u33[9] a; (* unused *) u1 b = a[2-:1]",
-             List((ExprCast(TypeUInt(4), 2), ExprCast(TypeUInt(4), 1)))),
-//            ("u32[8] a; (* unused *) u2 b = a[5:0][3:2]",
-//             List((ExprCast(TypeUInt(5), 5), ExprCast(TypeUInt(5), 0)),
-//                  (ExprCast(TypeUInt(4), 3), ExprCast(TypeUInt(3), 1)))),
-//            ("u33[9] a; (* unused *) u2 b = a[5:0][3:2]",
-//             List((ExprCast(TypeUInt(6), 5), ExprCast(TypeUInt(6), 0)),
-//                  (ExprCast(TypeUInt(4), 3), ExprCast(TypeUInt(4), 1)))),
-//            ("u32[8] a; (* unused *) u2 b = a[2+:1][3:2]",
-//             List((ExprCast(TypeUInt(5), 1), ExprCast(TypeUInt(5), 1)),
-//                  (ExprCast(TypeUInt(3), 1), ExprCast(TypeUInt(3), 1)))),
-//            ("u33[9] a; (* unused *) u2 b = a[2+:1][3:2]",
-//             List((ExprCast(TypeUInt(6), 1), ExprCast(TypeUInt(6), 1)),
-//                  (ExprCast(TypeUInt(4), 1), ExprCast(TypeUInt(4), 1)))),
-//            ("u32[8] a; (* unused *) u2 b = a[2-:1][3:2]",
-//             List((ExprCast(TypeUInt(5), 1), ExprCast(TypeUInt(5), 1)),
-//                  (ExprCast(TypeUInt(3), 1), ExprCast(TypeUInt(3), 1)))),
-//            ("u33[9] a; (* unused *) u2 b = a[2-:1][3:2]",
-//             List((ExprCast(TypeUInt(6), 1), ExprCast(TypeUInt(6), 1)),
-//                  (ExprCast(TypeUInt(4), 1), ExprCast(TypeUInt(4), 1)))),
+            // format: off
+            ("u8 a; (* unused *) u2 b = a[1:0]", List((ExprCast(TypeUInt(3), 1), ExprCast(TypeUInt(3), 0)))),
+            ("u9 a; (* unused *) u2 b = a[1:0]", List((ExprCast(TypeUInt(4), 1), ExprCast(TypeUInt(4), 0)))),
+            ("u8 a; (* unused *) u1 b = a[2+:1]", List((ExprCast(TypeUInt(3), 2), ExprCast(TypeUInt(3), 1)))),
+            ("u9 a; (* unused *) u1 b = a[2+:1]", List((ExprCast(TypeUInt(4), 2), ExprCast(TypeUInt(4), 1)))),
+            ("u8 a; (* unused *) u1 b = a[2-:1]", List((ExprCast(TypeUInt(3), 2), ExprCast(TypeUInt(3), 1)))),
+            ("u9 a; (* unused *) u1 b = a[2-:1]", List((ExprCast(TypeUInt(4), 2), ExprCast(TypeUInt(4), 1)))),
+            ("u32[8] a; (* unused *) u2 b = a[1:0]", List((ExprCast(TypeUInt(3), 1), ExprCast(TypeUInt(3), 0)))),
+            ("u33[9] a; (* unused *) u2 b = a[1:0]", List((ExprCast(TypeUInt(4), 1), ExprCast(TypeUInt(4), 0)))),
+            ("u32[8] a; (* unused *) u1 b = a[2+:1]", List((ExprCast(TypeUInt(3), 2), ExprCast(TypeUInt(3), 1)))),
+            ("u33[9] a; (* unused *) u1 b = a[2+:1]", List((ExprCast(TypeUInt(4), 2), ExprCast(TypeUInt(4), 1)))),
+            ("u32[8] a; (* unused *) u1 b = a[2-:1]", List((ExprCast(TypeUInt(3), 2), ExprCast(TypeUInt(3), 1)))),
+            ("u33[9] a; (* unused *) u1 b = a[2-:1]", List((ExprCast(TypeUInt(4), 2), ExprCast(TypeUInt(4), 1)))),
+            // format: on
           )
         } {
           slice in {
@@ -211,8 +187,8 @@ final class AddImplicitCastsSpec extends FreeSpec with AlogicTest {
         for {
           (decl, res) <- List(
             ("(* unused *) i8 a = 'sd2", ExprCast(TypeSInt(8), ExprNum(true, 2))),
-            ("(* unused *) u8 a = 'sd2", ExprCast(TypeUInt(8), ExprNum(true, 2))),
-            ("(* unused *) i7 a = 2", ExprCast(TypeSInt(7), ExprNum(false, 2))),
+            ("(* unused *) u8 a = 'sd2", ExprCast(TypeSInt(8), ExprNum(true, 2))),
+            ("(* unused *) i7 a = 2", ExprCast(TypeUInt(7), ExprNum(false, 2))),
             ("(* unused *) u7 a = 2", ExprCast(TypeUInt(7), ExprNum(false, 2))),
             ("(* unused *) param int a = 8'd2", ExprCast(TypeNum(true), ExprInt(false, 8, 2))),
             ("(* unused *) param uint a = 8'd2", ExprCast(TypeNum(false), ExprInt(false, 8, 2))),
@@ -239,12 +215,12 @@ final class AddImplicitCastsSpec extends FreeSpec with AlogicTest {
         for {
           (assign, res) <- List(
             ("i8 a = 0; a = 'sd2", ExprCast(TypeSInt(8), ExprNum(true, 2))),
-            ("u8 a = 0; a = 'sd2", ExprCast(TypeUInt(8), ExprNum(true, 2))),
-            ("i7 a = 0; a = 2", ExprCast(TypeSInt(7), ExprNum(false, 2))),
+            ("u8 a = 0; a = 'sd2", ExprCast(TypeSInt(8), ExprNum(true, 2))),
+            ("i7 a = 0; a = 2", ExprCast(TypeUInt(7), ExprNum(false, 2))),
             ("u7 a = 0; a = 2", ExprCast(TypeUInt(7), ExprNum(false, 2))),
             ("i8 a = 0; a += 'sd2", ExprCast(TypeSInt(8), ExprNum(true, 2))),
-            ("u8 a = 0; a += 'sd2", ExprCast(TypeUInt(8), ExprNum(true, 2))),
-            ("i7 a = 0; a += 2", ExprCast(TypeSInt(7), ExprNum(false, 2))),
+            ("u8 a = 0; a += 'sd2", ExprCast(TypeSInt(8), ExprNum(true, 2))),
+            ("i7 a = 0; a += 2", ExprCast(TypeUInt(7), ExprNum(false, 2))),
             ("u7 a = 0; a += 2", ExprCast(TypeUInt(7), ExprNum(false, 2)))
           )
         } {
