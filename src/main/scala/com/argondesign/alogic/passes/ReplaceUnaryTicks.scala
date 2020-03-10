4a0f1f18c18ba9d1c119f52cd7cac55977914bcb
Geza Lore <gezalore@gmail.com>
2020-03-10 17:26:17 +0000

    Make the ReplaceUnaryTicks transform stateless

--- src/main/scala/com/argondesign/alogic/passes/ReplaceUnaryTicks.scala
+++ src/main/scala/com/argondesign/alogic/passes/ReplaceUnaryTicks.scala
@@ -22,33 +22,20 @@ package com.argondesign.alogic.passes
 import com.argondesign.alogic.ast.TreeTransformer
 import com.argondesign.alogic.ast.Trees._
 import com.argondesign.alogic.core.CompilerContext
-import com.argondesign.alogic.core.Types.TypeFund
 import com.argondesign.alogic.typer.TypeAssigner
-
-import scala.collection.mutable
 
 final class ReplaceUnaryTicks(implicit cc: CompilerContext) extends TreeTransformer {
 
-  override val typed = false
+  override protected def enter(tree: Tree): Option[Tree] = tree match {
+    case expr @ ExprUnary("'", op) =>
+      Some {
+        TypeAssigner(ExprCast(expr.tpe.asFund, walk(op).asInstanceOf[Expr]) withLoc tree.loc)
+      }
 
-  private val kindStack = mutable.Stack[TypeFund]()
-
-  override protected def enter(tree: Tree): Option[Tree] = {
-    tree match {
-      case expr @ ExprUnary("'", _) => kindStack push expr.tpe.asFund
-      case _                        =>
-    }
-    None
-  }
-
-  override def transform(tree: Tree): Tree = tree match {
-    case ExprUnary("'", op)  => TypeAssigner(ExprCast(kindStack.pop(), op) withLoc tree.loc)
-    case node if node.hasTpe => tree
-    case node                => TypeAssigner(node)
+    case _ => None
   }
 
   override protected def finalCheck(tree: Tree): Unit = {
-    assert(kindStack.isEmpty)
     assert(tree forallAll { case node: Tree => node.hasTpe })
     assert((tree collect { case node @ ExprUnary("'", _) => node }).isEmpty)
   }
