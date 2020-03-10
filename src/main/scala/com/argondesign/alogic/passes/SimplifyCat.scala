d691ac69804a28e3e485e79e6f1548bb4cdd28d3
Geza Lore <gezalore@gmail.com>
2020-03-10 16:32:19 +0000

    Freshen SimplifyCat
    - Remove checks for properties of widths now ensured by the Typer
    - Walk results recursively to simplify nested cats

--- src/main/scala/com/argondesign/alogic/passes/SimplifyCat.scala
+++ src/main/scala/com/argondesign/alogic/passes/SimplifyCat.scala
@@ -22,6 +22,7 @@ import com.argondesign.alogic.ast.Trees._
 import com.argondesign.alogic.core.CompilerContext
 import com.argondesign.alogic.core.Loc
 import com.argondesign.alogic.core.Symbols.Symbol
+import com.argondesign.alogic.typer.TypeAssigner
 import com.argondesign.alogic.util.BigIntOps._
 import com.argondesign.alogic.util.unreachable
 
@@ -38,103 +39,105 @@ final class SimplifyCat(implicit cc: CompilerContext) extends TreeTransformer {
   ): List[(List[Expr], List[Expr])] = {
     def width(es: List[Expr]) = (es map { _.tpe.width }).sum
 
-    if (width(as) != width(bs)) {
-      // TODO: this check should be performed in the Typer,
-      // when we have agreed on proper widths semantics
-      cc.error(
-        loc,
-        "Widths do not match",
-        s"left hand side is ${width(as)}-bits wide",
-        s"right hand side is ${width(bs)}-bits wide"
-      )
-      List((as, bs))
-    } else {
-      val pairs = ListBuffer[(List[Expr], List[Expr])]()
+    assert(width(as) == width(bs))
+    val pairs = ListBuffer[(List[Expr], List[Expr])]()
 
-      @tailrec
-      def loop(suba: List[Expr], subb: List[Expr], as: List[Expr], bs: List[Expr]): Unit = {
-        val aw = width(suba)
-        val bw = width(subb)
-        if (aw == bw) {
-          pairs.append((suba.reverse, subb.reverse))
-          (as, bs) match {
-            case (Nil, Nil)         => ()
-            case (a :: at, b :: bt) => loop(List(a), List(b), at, bt)
-            case _                  => unreachable
+    @tailrec
+    def loop(suba: List[Expr], subb: List[Expr], as: List[Expr], bs: List[Expr]): Unit = {
+      val aw = width(suba)
+      val bw = width(subb)
+      if (aw == bw) {
+        pairs.append((suba.reverse, subb.reverse))
+        (as, bs) match {
+          case (Nil, Nil)         => ()
+          case (a :: at, b :: bt) => loop(List(a), List(b), at, bt)
+          case _                  => unreachable
+        }
+      } else if (aw < bw) {
+        loop(as.head :: suba, subb, as.tail, bs)
+      } else {
+        loop(suba, bs.head :: subb, as, bs.tail)
+      }
+    }
+
+    loop(List(as.head), List(bs.head), as.tail, bs.tail)
+
+    pairs.toList
+  }
+
+  override def transform(tree: Tree): Tree = tree match {
+    case StmtAssign(ExprCat(parts), ExprInt(_, _, value)) =>
+      val widths = parts map { _.tpe.width.toInt }
+      val lsbs = widths.scanRight(BigInt(0))(_ + _).tail map { _.toInt }
+      Thicket {
+        List from {
+          (parts lazyZip widths lazyZip lsbs) flatMap {
+            case (expr, width, lsb) =>
+              val signed = expr.tpe.isSigned
+              val rhs = TypeAssigner {
+                ExprInt(signed, width.toInt, value.extract(lsb, width, signed)) withLoc tree.loc
+              }
+              walk(TypeAssigner(StmtAssign(expr, rhs) withLoc tree.loc)) match {
+                case Thicket(ts) => ts
+                case t           => List(t)
+              }
           }
-        } else if (aw < bw) {
-          loop(as.head :: suba, subb, as.tail, bs)
-        } else {
-          loop(suba, bs.head :: subb, as, bs.tail)
         }
       }
 
-      loop(List(as.head), List(bs.head), as.tail, bs.tail)
-
-      pairs.toList
-    }
-  }
-
-  override def transform(tree: Tree): Tree = {
-    val result = tree match {
-
-      case StmtAssign(ExprCat(parts), ExprInt(_, width, value)) =>
-        val widths = parts map { _.tpe.width.toInt }
-        if (widths.sum != width) {
-          tree
-        } else {
-          val lsbs = widths.scanRight(BigInt(0))(_ + _).tail map { _.toInt }
-          StmtBlock {
-            (for ((expr, width, lsb) <- parts lazyZip widths lazyZip lsbs) yield {
-              val signed = expr.tpe.isSigned
-              StmtAssign(expr, ExprInt(signed, width.toInt, value.extract(lsb, width, signed)))
-            }).toList
+    case StmtAssign(ExprCat(oLhss), ExprCat(oRhss)) =>
+      // Do not simplify if a symbol appears on both sides (eg {a, b} = {b, a})
+      // as in this case the rhs must be read atomically
+      val lSymbols = (oLhss collect { case ExprSym(symbol) => symbol }).toSet
+      val rSymbols = (oRhss collect { case ExprSym(symbol) => symbol }).toSet
+      lazy val pairs = pairUp(tree.loc, oLhss, oRhss)
+      if ((lSymbols intersect rSymbols).nonEmpty || pairs.lengthIs == 1) {
+        tree
+      } else {
+        Thicket {
+          pairs flatMap {
+            case (lhss, rhss) =>
+              val lhs = lhss match {
+                case head :: Nil => head
+                case _           => TypeAssigner(ExprCat(lhss) withLoc tree.loc)
+              }
+              val rhs = rhss match {
+                case head :: Nil => head
+                case _           => TypeAssigner(ExprCat(rhss) withLoc tree.loc)
+              }
+              walk(TypeAssigner(StmtAssign(lhs, rhs) withLoc tree.loc)) match {
+                case Thicket(ts) => ts
+                case t           => List(t)
+              }
           }
         }
+      }
 
-      case StmtAssign(ExprCat(oLhss), ExprCat(oRhss)) =>
-        // Do not simplify if a symbol appears on both sides (eg {a, b} = {b, a})
-        // as in this case the rhs must be read atomically
-        val lSymbols = (oLhss collect { case ExprSym(symbol) => symbol }).toSet
-        val rSymbols = (oRhss collect { case ExprSym(symbol) => symbol }).toSet
-
-        if ((lSymbols intersect rSymbols).isEmpty) {
-          val pairs = pairUp(tree.loc, oLhss, oRhss)
-          if (pairs.lengthCompare(1) == 0) {
-            tree
-          } else {
-            val assigns = for ((lhss, rhss) <- pairs) yield {
-              val lhs = if (lhss.lengthCompare(1) == 0) lhss.head else ExprCat(lhss)
-              val rhs = if (rhss.lengthCompare(1) == 0) rhss.head else ExprCat(rhss)
-              StmtAssign(lhs, rhs)
-            }
-            StmtBlock(assigns)
+    case EntConnect(ExprCat(oLhss), List(ExprCat(oRhss))) =>
+      val pairs = pairUp(tree.loc, oLhss, oRhss)
+      if (pairs.lengthIs == 1) {
+        tree
+      } else {
+        Thicket {
+          pairs flatMap {
+            case (lhss, rhss) =>
+              val lhs = lhss match {
+                case head :: Nil => head
+                case _           => TypeAssigner(ExprCat(lhss) withLoc tree.loc)
+              }
+              val rhs = rhss match {
+                case head :: Nil => head
+                case _           => TypeAssigner(ExprCat(rhss) withLoc tree.loc)
+              }
+              walk(TypeAssigner(EntConnect(lhs, List(rhs)) withLoc tree.loc)) match {
+                case Thicket(ts) => ts
+                case t           => List(t)
+              }
           }
-        } else {
-          tree
         }
+      }
 
-      case EntConnect(ExprCat(oLhss), List(ExprCat(oRhss))) =>
-        val pairs = pairUp(tree.loc, oLhss, oRhss)
-        if (pairs.lengthCompare(1) == 0) {
-          tree
-        } else {
-          val assigns = for ((lhss, rhss) <- pairs) yield {
-            val lhs = if (lhss.lengthCompare(1) == 0) lhss.head else ExprCat(lhss)
-            val rhs = if (rhss.lengthCompare(1) == 0) rhss.head else ExprCat(rhss)
-            EntConnect(lhs, List(rhs))
-          }
-          Thicket(assigns)
-        }
-
-      case _ => tree
-    }
-
-    if (result ne tree) {
-      result regularize tree.loc
-    }
-
-    result
+    case _ => tree
   }
 
 }
