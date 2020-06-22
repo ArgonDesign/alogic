76ce7340518141f65688a27905beeaf8fe6226fc
Geza Lore <gezalore@gmail.com>
2020-06-22 08:41:40 +0100

    Ensure output symbols are not target language keywords

--- src/main/scala/com/argondesign/alogic/passes/RenameSymbols.scala
+++ src/main/scala/com/argondesign/alogic/passes/RenameSymbols.scala
@@ -26,6 +26,49 @@ import scala.collection.mutable
 object RenameSymbols extends PairsTransformerPass {
   val name = "rename-symbols"
 
+  // format: off
+  // IEEE 1800-2017 Annex B
+  private val systemVerilogKeywords = Set(
+    "accept_on", "alias", "always", "always_comb", "always_ff", "always_latch",
+    "and", "assert", "assign", "assume", "automatic", "before", "begin", "bind",
+    "bins", "binsof", "bit", "break", "buf", "bufif0", "bufif1", "byte", "case",
+    "casex", "casez", "cell", "chandle", "checker", "class", "clocking", "cmos",
+    "config", "const", "constraint", "context", "continue", "cover",
+    "covergroup", "coverpoint", "cross", "deassign", "default", "defparam",
+    "design", "disable", "dist", "do", "edge", "else", "end", "endcase",
+    "endchecker", "endclass", "endclocking", "endconfig", "endfunction",
+    "endgenerate", "endgroup", "endinterface", "endmodule", "endpackage",
+    "endprimitive", "endprogram", "endproperty", "endsequence", "endspecify",
+    "endtable", "endtask", "enum", "event", "eventually", "expect", "export",
+    "extends", "extern", "final", "first_match", "for", "force", "foreach",
+    "forever", "fork", "forkjoin", "function", "generate", "genvar", "global",
+    "highz0", "highz1", "if", "iff", "ifnone", "ignore_bins", "illegal_bins",
+    "implements", "implies", "import", "incdir", "include", "initial", "inout",
+    "input", "inside", "instance", "int", "integer", "interconnect",
+    "interface", "intersect", "join", "join_any", "join_none", "large", "let",
+    "liblist", "library", "local", "localparam", "logic", "longint",
+    "macromodule", "matches", "medium", "modport", "module", "nand", "negedge",
+    "nettype", "new", "nexttime", "nmos", "nor", "noshowcancelled", "not",
+    "notif0", "notif1", "null", "or", "output", "package", "packed",
+    "parameter", "pmos", "posedge", "primitive", "priority", "program",
+    "property", "protected", "pull0", "pull1", "pulldown", "pullup",
+    "pulsestyle_ondetect", "pulsestyle_onevent", "pure", "rand", "randc",
+    "randcase", "randsequence", "rcmos", "real", "realtime", "ref", "reg",
+    "reject_on", "release", "repeat", "restrict", "return", "rnmos", "rpmos",
+    "rtran", "rtranif0", "rtranif1", "s_always", "scalared", "sequence",
+    "s_eventually", "shortint", "shortreal", "showcancelled", "signed", "small",
+    "s_nexttime", "soft", "solve", "specify", "specparam", "static", "string",
+    "strong", "strong0", "strong1", "struct", "s_until", "s_until_with",
+    "super", "supply0", "supply1", "sync_accept_on", "sync_reject_on", "table",
+    "tagged", "task", "this", "throughout", "time", "timeprecision", "timeunit",
+    "tran", "tranif0", "tranif1", "tri", "tri0", "tri1", "triand", "trior",
+    "trireg", "type", "typedef", "union", "unique", "unique0", "unsigned",
+    "until", "until_with", "untyped", "use", "uwire", "var", "vectored",
+    "virtual", "void", "wait", "wait_order", "wand", "weak", "weak0", "weak1",
+    "while", "wildcard", "wire", "with", "within", "wor", "xnor", "xor"
+  )
+  // format: on
+
   override protected def process(
       input: List[(Decl, Defn)]
     )(
@@ -34,6 +77,14 @@ object RenameSymbols extends PairsTransformerPass {
     ): List[(Decl, Defn)] = {
 
     val topNames = mutable.Set[String]()
+
+    def fixIfKeyword(name: String): String = {
+      if (systemVerilogKeywords contains name) {
+        name + "_" // Add a single underscore
+      } else {
+        name
+      }
+    }
 
     // Rename symbol within entities that have the same name
     input.iterator collect {
@@ -74,13 +125,19 @@ object RenameSymbols extends PairsTransformerPass {
           }
         }
 
+        // Fix keywords
+        decls foreach { decl =>
+          decl.symbol.name = fixIfKeyword(decl.symbol.name)
+        }
+
         // Add entity prefix
         val ep = cc.settings.ensurePrefix
         val prefix = (0 to ep.length) collectFirst {
           case n if eSymbol.name startsWith ep.drop(n) => ep.take(n)
         }
 
-        val nameWithPrefix = prefix.get + eSymbol.name
+        // Also ensure not keyword
+        val nameWithPrefix = fixIfKeyword(prefix.get + eSymbol.name)
 
         // Enforce max name length if provided
         val entityNameWithLegalLen = cc.settings.outputNameMaxLength match {
