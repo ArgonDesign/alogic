f5e80956b685a5af01bff0c99811f8c65533c0fb
Geza Lore <gezalore@gmail.com>
2021-01-27 07:25:43 +0000

    Minor cleanup

--- src/main/scala/com/argondesign/alogic/frontend/Evaluate.scala
+++ src/main/scala/com/argondesign/alogic/frontend/Evaluate.scala
@@ -1,11 +1,11 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2017-2020 Argon Design Ltd. All rights reserved.
+// Copyright (c) 2017-2021 Argon Design Ltd. All rights reserved.
 //
 // This file is covered by the BSD (with attribution) license.
 // See the LICENSE file for the precise wording of the license.
 //
 // DESCRIPTION:
-//  Compute value of expressions/symbols.
+// Compute value of expressions/symbols.
 ////////////////////////////////////////////////////////////////////////////////
 
 package com.argondesign.alogic.frontend
@@ -13,7 +13,6 @@ package com.argondesign.alogic.frontend
 import com.argondesign.alogic.ast.StatelessTreeTransformer
 import com.argondesign.alogic.ast.Trees._
 import com.argondesign.alogic.core.Loc
-import com.argondesign.alogic.core.Messages.Ice
 import com.argondesign.alogic.core.Symbols.Symbol
 import com.argondesign.alogic.util.unreachable
 import com.argondesign.alogic.util.BigIntOps.BigIntClassOps
@@ -22,64 +21,46 @@ import scala.util.chaining.scalaUtilChainingOps
 
 private[frontend] object Evaluate {
 
-  def apply(
-      expr: Expr,
-      hint: => String
-    )(
-      implicit
-      fe: Frontend
-    ): FinalResult[BigInt] =
+  def apply(expr: Expr, hint: => String)(implicit fe: Frontend): FinalResult[BigInt] =
     fe.typeCheck(expr) flatMap { _ =>
-      var bad: Option[FinalResult[BigInt]] = None
-
       object Transform extends StatelessTreeTransformer {
-        // Bail quickly, if there was an issue..
-        override def skip(tree: Tree): Boolean = bad.isDefined
+        // Replaces references to symbols with their value. We do it via the
+        // frontend for 2 reasons. One is to track dependencies for the
+        // circularity check, but more importantly the definitions have not
+        // been through Clarify yet, so SimplifyExpr cannot deal with them.
+
+        var bad: Option[FinalResult[BigInt]] = None
 
         private def evaluate(symbol: Symbol, loc: Loc): Option[Expr] =
           fe.evaluate(symbol, loc, hint) match {
-            case Complete(e) =>
-              // Walk recursively so there are no symbols left anywhere
-              Some(walkSame(e))
-            case failure: Failure =>
-              bad = Some(failure)
-              None
-            case unknown: Unknown =>
-              bad = Some(unknown)
-              None
+            // Walk recursively so there are no symbols left anywhere
+            case Complete(e)      => Some(walkSame(e))
+            case failure: Failure => bad = Some(failure); None
+            case unknown: Unknown => bad = Some(unknown); None
           }
 
-        // Replace references to symbols with their value (we do it via the
-        // frontend for 2 reasons. One is to track dependencies, but more
-        // importantly the definitions haven't been through Clarify yet, so
-        // the standard constant folder cannot deal with them yet).
+        override def skip(tree: Tree): Boolean = bad.isDefined // Bail quickly, on issue
 
         override protected def enter(tree: Tree): Option[Tree] = tree match {
-          case _: ExprDot => unreachable
+          case ExprSym(symbol) => evaluate(symbol, tree.loc) orElse Some(tree)
           case ExprSymSel(_, tSymbol) =>
             tSymbol.desc match {
               case _: DescVal | _: DescParam | _: DescConst | _: DescGenVar =>
-                evaluate(tSymbol, tree.loc)
+                evaluate(tSymbol, tree.loc) orElse Some(tree)
               case DescAlias(_, _, expr, _) => Some(walk(expr))
               case _                        => None
             }
-          case _ => None
-        }
-
-        override def transform(tree: Tree): Tree = tree match {
-          case ExprSym(symbol) => evaluate(symbol, tree.loc) getOrElse tree
-          case _: ExprIdent    => throw Ice(tree, "Attempting to evaluate unresolved reference")
-          case _               => tree
+          case _: ExprIdent | _: ExprDot | _: ExprSel => unreachable
+          case _                                      => None
         }
       }
 
-      Clarify(expr) pipe { clarified =>
-        val transformed = clarified rewrite Transform
-        bad getOrElse {
-          transformed.valueOption match {
-            case Some(v) => Complete(v)
-            case None    => Failure(expr, s"${hint.capitalize} must be a compile time constant")
-          }
+      val clarified = Clarify(expr)
+      val transformed = clarified rewrite Transform
+      Transform.bad getOrElse {
+        transformed.valueOption match {
+          case Some(v) => Complete(v)
+          case None    => Failure(expr, s"${hint.capitalize} must be a compile time constant")
         }
       }
     }
